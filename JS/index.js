f5abf6f6155207380eddc3c48c703648c5a69588
Danielcode <danielalejndro@gmail.com>
2021-11-06 14:02:01 -0400

    Adding async-await

--- JS/index.js
+++ JS/index.js
@@ -3,13 +3,28 @@ import { fetchData } from './fetchData.js';
 import { cardBuilder } from './cardBuild.js';
 const promiseContainer = document.getElementById('promise');
 
-fetchData(API)
-.then(data =>{
-    data.results.map(character =>{
-        const cards = cardBuilder(character);
-        promiseContainer.appendChild(cards);
-    })
-})
-.catch (error => {
-    return error;
-})
+// fetchData(API)
+// .then(data =>{
+//     data.results.map(character =>{
+//         const cards = cardBuilder(character);
+//         promiseContainer.appendChild(cards);
+//     })
+// })
+// .catch (error => {
+//     return error;
+// })
+
+const requestApi = async (url) =>{
+    try{
+        const data = await fetchData(url)
+        fetchData(data.results.map(character =>{
+                    const cards = cardBuilder(character);
+                    promiseContainer.appendChild(cards);
+                }))
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+requestApi(API);
